5057e3c0926173b4fb8e7087267f510be6972376
thayakawa <thayakawa@hayakawatakuto-no-iMac.local>
2014-08-01 11:38:22 +0900

    宣言する値をまとめるヘッダを作成

---
+++ include/define.h
@@ -0,0 +1,18 @@
+#ifndef INCLUDE_DEFINE_H_
+#define INCLUDE_DEFINE_H_
+
+
+// 試行回数
+#define NTRY 10000000
+
+// 排出確率
+#define FIRST 60
+#define SECOND 30
+#define THIRD 7
+#define FOURTH 2
+#define FIFTH 1
+
+
+
+#endif  // INCLUDE_DEFINE_H_
+
